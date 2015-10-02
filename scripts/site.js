d73cc54e957d411db64051cc5d958feee1b890b9
Chris Russo <chris.andrews.russo@gmail.com>
2015-10-01 21:38:06 -0400

    Add js code to test clicking right in gallery

--- scripts/site.js
+++ scripts/site.js
@@ -493,3 +493,14 @@ Y.use('node', 'squarespace-gallery-ng' ,'squarespace-image-loader', function(Y) 
   });
 
 });
+
+// Test right clicking on.
+jQuery('.overlay-controls.right-control').click(function(event){
+  event.preventDefault();
+  var activeSlideImageID = jQuery('.sqs-active-slide img').attr('data-image-id');
+  if (activeSlideImageID == '560038d8e4b052586769796c') {
+    var domain = location.protocol + "//" + location.host
+    window.location.href = domain + "/travel";
+  }
+});
+
