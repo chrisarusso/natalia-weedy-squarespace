24e9b8678b0bd83ba678310780152368e22b8d0b
Chris Russo <chris.andrews.russo@gmail.com>
2015-10-01 23:39:35 -0400

    Make galleries seamless between each other
    Navigate through galleries as if they
    were all loaded from the same page
    
    This was the result of a ton of trial and error commits
    ending with
    e98fb493504ae7dc10a60da9c674e80878c9ff52

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,11 +1,60 @@
+var firstSlideID = $('#slideshow').children().first().attr('data-slide-id');
+var lastSlideID = $('#slideshow').children().last().attr('data-slide-id');
+var currentGallery =  Static.SQUARESPACE_CONTEXT.collection.title;
+var galleries = [
+  "Life",
+  "Travel",
+  "Land",
+  "Enthusiasts",
+  "Editorial",
+  "Faces",
+  "Work",
+  "Body",
+  "Icescapes"
+];
 
-// Test right clicking on.
-$('.overlay-controls.right-control').click(function(event){
-  event.preventDefault();
-  var activeSlideImageID = $('.sqs-active-slide img').attr('data-image-id');
-  if (activeSlideImageID == '560038d8e4b052586769796c') {
-    var domain = location.protocol + "//" + location.host
-    window.location.href = domain + "/travel";
+var arrayLength = galleries.length;
+var previousGallery = '';
+var nextGallery = '';
+
+for (var i = 0; i < arrayLength; i++) {
+  if (currentGallery == galleries[i]){
+    if (i == 0) {
+      // We have no previous
+      previousGallery = null;
+    }
+    else if (i == arrayLength - 1) {
+      nextGallery = null;
+    }
+
+    if (nextGallery != null) {
+      nextGallery = galleries[i + 1];
+    }
+
+    if (previousGallery != null) {
+      previousGallery = galleries[i - 1];
+    }
+  }
+}
+
+$('.overlay-controls.right-control').on('click', function(event){
+  // This prevents the image from the next image from loading
+  event.stopImmediatePropagation();
+  var activeSlideID = $('.sqs-active-slide').attr('data-slide-id');
+  if (activeSlideID == lastSlideID) {
+    var domain = location.protocol + "//" + location.host;
+    window.location.href = domain + "/" + nextGallery.toLowerCase();
+  }
+  return false;
+});
+
+$('.overlay-controls.left-control').click(function(event){
+  // This prevents the image from the next image from loading
+  event.stopImmediatePropagation();
+  var activeSlideID = $('.sqs-active-slide').attr('data-slide-id');
+  if (activeSlideID == firstSlideID && previousGallery != null) {
+    var domain = location.protocol + "//" + location.host;
+    window.location.replace(domain + "/" + previousGallery.toLowerCase());
   }
 });
 
