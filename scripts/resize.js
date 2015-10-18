4fb768c509f59b9091ca96fec5d8a85c4f8c4cb7
Chris Russo <chris.andrews.russo@gmail.com>
2015-10-18 17:24:24 -0400

    Add resize.js to test limiting resizing of images
    We're trying to stop image stretching

---
+++ scripts/resize.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    /* $('#slideshow img').each(function(){
+        //var width = $(this).prop('naturalWidth');
+        //console.log('this is ' + this.prop('naturalWidth'));
+        console.log('this is ' + $(this).prop('naturalWidth'));
+    }); */
+});
+
+// Perhaps the next move would be to listen for resize
+// and check width and height and if either are above, resize
+// image
+/*
+$( window ).resize(function() {
+    // This works
+
+    var activeImage = $('.sqs-active-slide img');
+    alert('active image natural width is ' + activeImage.prop('naturalWidth'));
+
+});
+     */
