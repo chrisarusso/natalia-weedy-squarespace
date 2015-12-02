505a3b5b154a39a6f92e2f972b3ab9969f062470
Chris Russo <chris.andrews.russo@gmail.com>
2015-12-01 23:03:37 -0500

    Revert "Comment this out for LF so she can grab images"
    This reverts commit 798f14505b0b9cfd9983648b93ce3a22864fe9aa.
    
    Lindsay is done, so we put it back to how it was

--- scripts/right-click.js
+++ scripts/right-click.js
@@ -1,7 +1,6 @@
 
 // Another option http://stackoverflow.com/questions/4753695/disabling-right-click-on-images-using-jquery
 $('.overlay-controls, img').bind('contextmenu', function(e) {
- // Comment this out so LF can access it for a bit
- // alert('Image protected');
- // return false;
+  alert('Image protected');
+  return false;
 });
