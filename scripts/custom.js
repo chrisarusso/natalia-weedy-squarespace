a8afa853634f5f02ad51dee666ed99015575e4f9
Chris Russo <chris.andrews.russo@gmail.com>
2016-01-26 16:07:46 -0500

    Get left not to work

--- scripts/custom.js
+++ scripts/custom.js
@@ -50,7 +50,7 @@ $('.overlay-controls.right-control').not('#collection-56a7b1b176d99c82e7d277a1 .
   return false;
 });
 
-$(".overlay-controls.left-control:not('#collection-56a7b1b176d99c82e7d277a1')").on('click', function(event){
+$('.overlay-controls.left-control').not('#collection-56a7b1b176d99c82e7d277a1 .overlay-controls.right-control').on('click', function(event){
 
     // This prevents the image from the next image from loading
     var activeSlideID = $('.sqs-active-slide').attr('data-slide-id');
